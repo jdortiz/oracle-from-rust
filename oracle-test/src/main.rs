93f8b95e37f94d44e46f1d74d4581a366eac8f57
Jorge D. Ortiz-Fuentes <jorge.ortiz.fuentes@oracle.com>
2026-01-16 17:51:25 +0100

    Add simple multi-row query

--- oracle-test/src/main.rs
+++ oracle-test/src/main.rs
@@ -1,6 +1,7 @@
 use oracle::Connection;
 
 fn main() -> Result<(), anyhow::Error> {
+    const MAX_ROWS: usize = 20;
     println!("oracle-test");
 
     let connection = Connection::connect("co", "co", "localhost:1521/FREEPDB1")?;
@@ -11,6 +12,16 @@ fn main() -> Result<(), anyhow::Error> {
     let row = connection.query_row("SELECT 'Hello Oracle!' FROM dual", &[])?;
     let greeting: String = row.get(0)?;
     println!("{greeting}");
+
+    let sql_customer_list = "SELECT * FROM co.customers";
+    println!("Getting {MAX_ROWS} customers:");
+    for row in connection.query(sql_customer_list, &[])?.take(MAX_ROWS) {
+        let row = row?;
+        let id: u32 = row.get(0)?;
+        let email: String = row.get(1)?;
+        let fullname: String = row.get(2)?;
+        println!("{id}: {fullname} - {email}");
+    }
 
     Ok(())
 }
