a5a27b2540f400af5ed4b7cf89c80abecf99d20b
Jorge D. Ortiz-Fuentes <jorge.ortiz.fuentes@oracle.com>
2026-01-21 12:06:39 +0100

    Add simple multi-row query with sibyl crate

--- sibyl-test/src/main.rs
+++ sibyl-test/src/main.rs
@@ -1,5 +1,6 @@
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+    const MAX_ROWS: usize = 20;
     println!("sibyl-test");
 
     let sibyl_env = sibyl::env()?;
@@ -17,6 +18,21 @@ async fn main() -> Result<(), anyhow::Error> {
         println!("{greeting}");
     } else {
         eprintln!("Greeting query failed.");
+    }
+
+    let sql_customer_list = "SELECT * FROM co.customers";
+    println!("Getting {MAX_ROWS} customers:");
+    let stmt = session.prepare(sql_customer_list).await?;
+    let rows = stmt.query(()).await?;
+    let mut i: usize = 0;
+    while i < MAX_ROWS
+        && let Some(row) = rows.next().await?
+    {
+        let id: u32 = row.get(0)?;
+        let email: String = row.get(1)?;
+        let fullname: String = row.get(2)?;
+        println!("{id}: {fullname} - {email}");
+        i += 1;
     }
 
     Ok(())
