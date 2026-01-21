485a7bc2dc65ff594ee2aff49408176d12897d32
Jorge D. Ortiz-Fuentes <jorge.ortiz.fuentes@oracle.com>
2026-01-21 11:54:57 +0100

    Connect to the database with sibyl crate

--- sibyl-test/src/main.rs
+++ sibyl-test/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    println!("sibyl-test");
+
+    let sibyl_env = sibyl::env()?;
+
+    let session = sibyl_env
+        .connect("localhost:1521/FREEPDB1", "co", "co")
+        .await?;
+
+    session.ping().await?;
+    println!("Connected to the database.");
+
+    Ok(())
 }
